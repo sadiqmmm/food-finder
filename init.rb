d200e60b971fdd57cd55f072646aa93b3909e2c6
Mohammed Sadiq <sadiqmmm@gmail.com>
2018-06-28 23:21:44 +0530

    initialize the init.rb

---
+++ init.rb
@@ -0,0 +1,8 @@
+APP_ROOT = File.dirname(__FILE__)
+
+# require "#{APP_ROOT}/lib/guide"
+# require File.join(APP_ROOT, 'lib', 'guide.rb')
+# require File.join(APP_ROOT, 'lib', 'guide')
+
+$LOAD_PATH.unshift(File.join(APP_ROOT, 'lib'))
+require 'guide'
